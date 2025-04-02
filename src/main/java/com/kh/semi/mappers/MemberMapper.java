4e40cd17452f07e1bba3dba247887f22c41cd259
jchxnee <hyeijoo1234@naver.com>
2025-04-02 17:06:41 +0900

    중복 회원가입 삭제

--- src/main/java/com/kh/semi/mappers/MemberMapper.java
+++ src/main/java/com/kh/semi/mappers/MemberMapper.java
@@ -14,6 +14,5 @@ public interface MemberMapper {
     ArrayList<Member> acceptMemberList();
     int approveMember(@Param("memId") String memId, @Param("storeSelect") String storeSelect);
     int rejectMember(@Param("memId") String memId);
-    ArrayList<Member> acceptMember();
-    int insertMember(Member member);
+
 }
--- src/main/java/com/kh/semi/service/MemberServiceImpl.java
+++ src/main/java/com/kh/semi/service/MemberServiceImpl.java
@@ -42,5 +42,4 @@ public class MemberServiceImpl implements MemberService {
         return memberMapper.rejectMember(memId);
     }
 
-
 }
