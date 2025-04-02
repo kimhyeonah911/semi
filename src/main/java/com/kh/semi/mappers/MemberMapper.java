4b7a94f5a464547bdb1645383773281594c9d335
kimhyeonah911 <haha020911@gmail.com>
2025-04-02 16:15:22 +0900

    회원가입1

--- src/main/java/com/kh/semi/mappers/MemberMapper.java
+++ src/main/java/com/kh/semi/mappers/MemberMapper.java
@@ -9,9 +9,12 @@ import java.util.ArrayList;
 @Mapper
 public interface MemberMapper {
     Member loginMember(String memId, String memPwd);
+    int insertMember(Member member);
+    int checkId(@Param("memId") String checkId);
 
     ArrayList<Member> selectMemberList();
     ArrayList<Member> acceptMemberList();
     int approveMember(@Param("memId") String memId, @Param("storeSelect") String storeSelect);
     int rejectMember(@Param("memId") String memId);
+
 }
