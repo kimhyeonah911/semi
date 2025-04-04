f934ae14630b62750aa40200ccd8555563c3e22e
jchxnee <hyeijoo1234@naver.com>
2025-04-04 12:27:32 +0900

    출/퇴근 버튼 생성 및 근태관리 클래스 생성

---
+++ src/main/java/com/kh/semi/domain/vo/Attendance.java
@@ -0,0 +1,20 @@
+package com.kh.semi.domain.vo;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Attendance {
+    private int attendanceNo;
+    private int empNo;
+    private Date workDay;
+    private Date clockIn;
+    private Date clockOut;
+    private String status;
+
+}
